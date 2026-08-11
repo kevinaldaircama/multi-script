0dc7f1d675271d1bfd0cd439fec9fa17b46048b4
kevinaldaircama <kevinaldaircamachoserna51@gmail.com>
2026-08-11 11:02:19 -0500

    Update badvpn.sh

--- protocolos/badvpn.sh
+++ protocolos/badvpn.sh
@@ -69,23 +69,6 @@ RestartSec=3
 [Install]
 WantedBy=multi-user.target
 EOF
-
-              
-    cat > /etc/systemd/system/$SERVICE2.service <<EOF              
-[Unit]              
-Description=BadVPN UDPGW Puerto 7200              
-After=network.target              
-              
-[Service]              
-Type=simple              
-ExecStart=$BIN --listen-addr 127.0.0.1:$PORT2 --max-clients 999 --max-connections-for-client 10              
-Restart=always              
-RestartSec=3              
-              
-[Install]              
-WantedBy=multi-user.target              
-EOF              
-              
     systemctl daemon-reload              
     systemctl enable $SERVICE1 $SERVICE2 >/dev/null 2>&1              
     systemctl restart $SERVICE1 $SERVICE2              
